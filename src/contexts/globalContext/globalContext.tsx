47e14166b8996b6fcb72b52d7074691476243b82
Travieso_jose <jose.travieso@vyaprojects.com>
2024-06-17 16:43:10 -0400

    chore: add GlobalContext

--- src/contexts/globalContext/globalContext.tsx
+++ src/contexts/globalContext/globalContext.tsx
@@ -1,1 +1,17 @@
+import { createContext, FC, ReactNode } from "react";
+import { itfGlobalContext } from "../../interfaces/ItfContexts";
 
+const defaultValues:itfGlobalContext =  {
+    
+}
+
+export const GlobalContext = createContext<itfGlobalContext>(defaultValues)
+
+
+export const GlobalContextProvider:FC<{children:ReactNode}> = ({children}) => {
+    
+    return <GlobalContext.Provider value={{}}>
+        {children}
+    </GlobalContext.Provider>
+
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { HashRouter } from 'react-router-dom'
+import { GlobalContextProvider } from './contexts/globalContext/globalContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App/>
-    </HashRouter>
-  </React.StrictMode>,
+    <GlobalContextProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </GlobalContextProvider>
+  </React.StrictMode>
 )
